a761597f7313f40acb841500b416aab11072c220
TwuanMinn <TwuanMinn@users.noreply.github.com>
2026-04-13 15:18:24 +0700

    feat: integrate aceternity ui hero-highlight framer-motion component

---
+++ src/components/ui/hero-highlight.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import { cn } from "../../lib/utils";
+import { motion } from "framer-motion";
+
+export const Highlight = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <motion.span
+      initial={{
+        backgroundSize: "0% 100%",
+      }}
+      whileInView={{
+        backgroundSize: "100% 100%",
+      }}
+      viewport={{ once: true }}
+      transition={{
+        duration: 2,
+        ease: "linear",
+        delay: 0.5,
+      }}
+      style={{
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "left center",
+        display: "inline",
+      }}
+      className={cn(
+        "relative inline-block pb-1 px-1 rounded-sm bg-gradient-to-r from-[#9ef3d6] to-[#9ef3d6]",
+        className
+      )}
+    >
+      {children}
+    </motion.span>
+  );
+};
